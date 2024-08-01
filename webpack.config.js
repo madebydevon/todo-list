43d88248280dc1899e1c1616eedff1e949f35e4c
Devon <174902334+madebydevon@users.noreply.github.com>
2024-08-01 21:24:01 +0200

    Minor changes to Webpack & NPM

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
+    mode: "development",
+    entry: "./src/index.js",
     output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: "./src/index.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
